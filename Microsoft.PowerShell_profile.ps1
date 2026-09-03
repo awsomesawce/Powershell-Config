178ca0727f8f1ccad64a4040c5e9a8232a516f57
Carl Capodice <69673640+awsomesawce@users.noreply.github.com>
2026-09-02 22:10:02 -0400

    refactor(profile): Refactor Microsoft.Powershell_profile.ps1
    Changed sal to Set-Alias: Use full cmdlet names and not the alias.
    Removed unnecessary and outdated code.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,30 +5,28 @@
 $env:POWERSHELL_TELEMETRY_OPTOUT = 1
 $env:PAGER = "less"
 $env:OLDPATH = $env:PATH.Clone()
-# Add ~/bin and python user-base scripts folder to PATH.
-# `pipenv` is installed ther
-# TODO: Use `pipx` to install `pipenv` in its own env.
-$env:PATH = "C:/Users/Carl/bin;$env:PATH"
-#$env:EDITOR = "codium"
-#$env:GIT_PAGER = 'delta'
-#$env:PATH = "C:/Users/Carl/AppData/Roaming/Python/Python311/Scripts;$env:PATH"
-# ! Add nmap's `npcap` to PATH so wireshark can work
-#$oldPath = $env:Path
-#$env:Path = "$env:USERPROFILE/scoop/apps/nmap/current;$env:Path"
 
-function Show-Welcome {
-    param([switch]$verb)
-    if ($verb) {
-        write-host -fore Magenta "`$verb is $verb"
+$env:PATH = "C:/Users/Carl/bin;$env:PATH"
+
+
+function Get-InstalledThemes {
+
+    <#
+.DESCRIPTION
+Gets installed oh-my-posh themes from scoop installed omp.
+#>
+
+    [cmdletbinding()]
+    param()
+
+    if (Test-Path $env:POSH_THEMES_PATH) {
+        Get-ChildItem $env:POSH_THEMES_PATH -ea SilentlyContinue
+    }
+    else {
+        Write-Error "Did not find oh-my-posh themes at $env:POSH_THEMES_PATH"
     }
 
-    Write-Host -fore cyan "Welcome to the REWRITE of the Powershell `$profile"
-    Write-Host -fore DarkCyan "`nNow I have to rewrite the entire profile and remember what alterations I did."
-
-    return $true
-
 }
-
 
 function which {
     <#.SYNOPSIS
@@ -55,27 +53,24 @@ function which {
 }
 
 
-Show-Welcome
-
-
 new-alias Prepare-Pipenv C:\Users\Carl\Documents\virt_env_test\pipenv_test\preparepipenv.ps1 -Description @"
 Adds location of ``pipenv`` to `$PATH
 "@ -Option None -Scope Local
 
 $Script:msyslocation = "C:\Users\Carl\scoop\apps\msys2\current"
 
-New-Alias -Name msysshell -Value "$msyslocation\msys2_shell.cmd" -Description "msys2_shell.cmd alias" -Option AllScope
+New-Alias -Name msysshell -Value "$msyslocation\msys2_shell.cmd" -Description "msys2_shell.cmd alias"
 
 New-Alias chtsh C:\Users\Carl\Documents\BASICS\getChtsh.ps1 `
     -Description "Chtsh function"
 
-Import-Module C:/Users/Carl/documents/BASICS/SearchTools/SearchTools.psd1
+Import-Module $env:USERPROFILE/BASICS/SearchTools/SearchTools.psd1
 
 new-alias g git -Description "Super short git invokation ftw.  1/3 the length!"
 
 
-sal pn pnpm
-sal fe ForEach-Object
-sal sel Select-Object
+Set-Alias pn pnpm
+Set-Alias fe ForEach-Object
+Set-Alias sel Select-Object
 
 oh-my-posh init pwsh --config C:\Users\Carl\scoop\apps\oh-my-posh\current\themes\sonicboom_dark.omp.json | invoke-expression
